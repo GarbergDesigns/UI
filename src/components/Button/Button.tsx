9ee30f305be559ef8246a12e122e6fd9f00cd603
Redstoneguy129 <public@cameronwhyte.me>
2023-02-16 21:48:39 +0000

    add Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+// @ts-ignore
+import ButtonStyles from "./Button.module.css";
+
+const sizeClassNames = {
+  small: "",
+  medium: "",
+  large: "",
+};
+
+const colourClassNames = {
+  primary: "",
+  secondary: "",
+  transparent: "",
+};
+
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  size?: keyof typeof sizeClassNames;
+  colour?: keyof typeof colourClassNames;
+};
+
+export const Button = ({
+  children,
+  size = "medium",
+  colour = "primary",
+  className = "",
+  ...props
+}: ButtonProps) => {
+  return (
+    <button className={`${className}`} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
